73e2f05c1ba8aa76c81a4546d79181dab4e70d1c
D3PI-CODE <d3pi.media@gmail.com>
2025-11-25 10:37:28 +0530

    removed useless commponents

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './login.css';
 import { NavLink, Navigate, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { tokens } from '@chakra-ui/react/theme';
 
 export default function Login() {
     const [password, setPassword] = useState('');
